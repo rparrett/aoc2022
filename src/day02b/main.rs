f19069647d115af7e3726f015860849100895f46
Rob Parrett <robparrett@gmail.com>
2022-12-02 12:50:26 -0700

    Day 2

---
+++ src/day02b/main.rs
@@ -0,0 +1,106 @@
+use anyhow::anyhow;
+use std::{cmp::Ordering, fs::File, io::BufRead, io::BufReader};
+
+struct Outcome(Ordering);
+
+#[derive(Eq, PartialEq)]
+enum Shape {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl Shape {
+    pub fn from_opponent_choice(choice: &str) -> anyhow::Result<Shape> {
+        match choice {
+            "A" => Ok(Shape::Rock),
+            "B" => Ok(Shape::Paper),
+            "C" => Ok(Shape::Scissors),
+            _ => Err(anyhow!("invalid opponent choice: {}", choice)),
+        }
+    }
+    pub fn get_choice_for_outcome(&self, outcome: &Outcome) -> Shape {
+        match (self, outcome.0) {
+            (Shape::Rock, Ordering::Equal) => Shape::Rock,
+            (Shape::Paper, Ordering::Equal) => Shape::Paper,
+            (Shape::Scissors, Ordering::Equal) => Shape::Scissors,
+            (Shape::Rock, Ordering::Less) => Shape::Scissors,
+            (Shape::Rock, Ordering::Greater) => Shape::Paper,
+            (Shape::Paper, Ordering::Less) => Shape::Rock,
+            (Shape::Paper, Ordering::Greater) => Shape::Scissors,
+            (Shape::Scissors, Ordering::Less) => Shape::Paper,
+            (Shape::Scissors, Ordering::Greater) => Shape::Rock,
+        }
+    }
+    pub fn score(&self) -> u32 {
+        match self {
+            Shape::Rock => 1,
+            Shape::Paper => 2,
+            Shape::Scissors => 3,
+        }
+    }
+}
+impl Ord for Shape {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (Shape::Rock, Shape::Rock)
+            | (Shape::Paper, Shape::Paper)
+            | (Shape::Scissors, Shape::Scissors) => Ordering::Equal,
+            (Shape::Rock, Shape::Paper) => Ordering::Less,
+            (Shape::Rock, Shape::Scissors) => Ordering::Greater,
+            (Shape::Paper, Shape::Rock) => Ordering::Greater,
+            (Shape::Paper, Shape::Scissors) => Ordering::Less,
+            (Shape::Scissors, Shape::Rock) => Ordering::Less,
+            (Shape::Scissors, Shape::Paper) => Ordering::Greater,
+        }
+    }
+}
+impl PartialOrd for Shape {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Outcome {
+    fn from_strategy_guide(outcome: &str) -> anyhow::Result<Self> {
+        match outcome {
+            "X" => Ok(Self(Ordering::Less)),
+            "Y" => Ok(Self(Ordering::Equal)),
+            "Z" => Ok(Self(Ordering::Greater)),
+            _ => Err(anyhow!("invalid outcome: {}", outcome)),
+        }
+    }
+}
+
+fn score_round(opponent_choice: &Shape, your_choice: &Shape) -> u32 {
+    (match your_choice.cmp(opponent_choice) {
+        Ordering::Less => 0,
+        Ordering::Equal => 3,
+        Ordering::Greater => 6,
+    }) + your_choice.score()
+}
+
+fn main() -> anyhow::Result<()> {
+    let mut total: u32 = 0;
+
+    let file = File::open("input/day02.txt")?;
+
+    for line in BufReader::new(file).lines().flatten() {
+        let (opponent, outcome_str) = line
+            .split_once(' ')
+            .ok_or_else(|| anyhow!("parsing failed"))?;
+
+        let opponent_choice = Shape::from_opponent_choice(opponent)?;
+        let outcome = Outcome::from_strategy_guide(outcome_str)?;
+
+        let your_choice = opponent_choice.get_choice_for_outcome(&outcome);
+
+        let score = score_round(&opponent_choice, &your_choice);
+
+        total += score;
+    }
+
+    println!("{}", total);
+
+    Ok(())
+}
