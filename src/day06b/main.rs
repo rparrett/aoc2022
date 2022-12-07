dfb951b40acb51d7c2cdf51a9410477e30ea1c53
Rob Parrett <robparrett@gmail.com>
2022-12-06 21:30:40 -0700

    Day 6

---
+++ src/day06b/main.rs
@@ -0,0 +1,20 @@
+use itertools::Itertools;
+use std::fs::read_to_string;
+
+const WINDOW_SIZE: usize = 14;
+
+fn main() -> anyhow::Result<()> {
+    let message = read_to_string("input/day06.txt")?;
+
+    for start in 0..(message.len() - WINDOW_SIZE) {
+        let end = start + WINDOW_SIZE;
+
+        let window = &message[start..end];
+        if !window.chars().combinations(2).any(|c| c[0] == c[1]) {
+            println!("{}", end);
+            break;
+        }
+    }
+
+    Ok(())
+}
