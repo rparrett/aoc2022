0737af640ff43984ef9858d4659d0e77ef03d8ae
Rob Parrett <robparrett@gmail.com>
2022-12-23 17:36:35 -0700

    Refactor day 8a

--- src/day08a/main.rs
+++ src/day08a/main.rs
@@ -24,6 +24,52 @@ impl Grid {
     fn zeros(rows: usize, cols: usize) -> Self {
         Grid(vec![vec![0; cols]; rows])
     }
+    fn enumerate_rows(
+        &self,
+    ) -> impl Iterator<Item = impl Iterator<Item = (usize, usize, &u32)> + '_> {
+        (0..self.rows())
+            .map(move |row| (0..self.cols()).map(move |col| (row, col, &self.0[row][col])))
+    }
+    fn enumerate_rev_rows(
+        &self,
+    ) -> impl Iterator<Item = impl Iterator<Item = (usize, usize, &u32)> + '_> {
+        (0..self.rows()).map(move |row| {
+            (0..self.cols())
+                .rev()
+                .map(move |col| (row, col, &self.0[row][col]))
+        })
+    }
+    fn enumerate_cols(
+        &self,
+    ) -> impl Iterator<Item = impl Iterator<Item = (usize, usize, &u32)> + '_> {
+        (0..self.cols())
+            .map(move |col| (0..self.rows()).map(move |row| (row, col, &self.0[row][col])))
+    }
+    fn enumerate_rev_cols(
+        &self,
+    ) -> impl Iterator<Item = impl Iterator<Item = (usize, usize, &u32)> + '_> {
+        (0..self.cols()).map(move |col| {
+            (0..self.rows())
+                .rev()
+                .map(move |row| (row, col, &self.0[row][col]))
+        })
+    }
+    fn iter(&self) -> impl Iterator<Item = &u32> + '_ {
+        self.0.iter().flat_map(|row| row.iter())
+    }
+}
+
+fn add_line_visibility<'a>(
+    line: impl Iterator<Item = (usize, usize, &'a u32)>,
+    visibility: &mut Grid,
+) {
+    let mut max: Option<u32> = None;
+    for (row, col, val) in line {
+        if max.filter(|max_v| *val <= *max_v).is_none() {
+            max = Some(*val);
+            visibility.0[row][col] += 1;
+        }
+    }
 }
 
 fn main() -> anyhow::Result<()> {
@@ -41,57 +87,21 @@ fn main() -> anyhow::Result<()> {
     let rows = grid.rows();
     let cols = grid.cols();
 
-    let mut visibility_grid = Grid::zeros(rows, cols);
+    let mut visibility = Grid::zeros(rows, cols);
 
-    for y in 0..rows {
-        // west -> east
-        let mut max: Option<u32> = None;
-        for x in 0..cols {
-            if max.filter(|v| grid.0[y][x] <= *v).is_none() {
-                max = Some(grid.0[y][x]);
-                visibility_grid.0[y][x] += 1;
-            }
-        }
-
-        // east -> west
-        let mut max: Option<u32> = None;
-        for x in (0..cols).rev() {
-            if max.filter(|v| grid.0[y][x] <= *v).is_none() {
-                max = Some(grid.0[y][x]);
-                visibility_grid.0[y][x] += 1;
-            }
-        }
-    }
-
-    for x in 0..cols {
-        // north -> south
-        let mut max: Option<u32> = None;
-        for y in 0..rows {
-            if max.filter(|v| grid.0[y][x] <= *v).is_none() {
-                max = Some(grid.0[y][x]);
-                visibility_grid.0[y][x] += 1;
-            }
-        }
-
-        // south -> north
-        let mut max: Option<u32> = None;
-        for y in (0..rows).rev() {
-            if max.filter(|v| grid.0[y][x] <= *v).is_none() {
-                max = Some(grid.0[y][x]);
-                visibility_grid.0[y][x] += 1;
-            }
-        }
-    }
+    grid.enumerate_rows()
+        .for_each(|line| add_line_visibility(line, &mut visibility));
+    grid.enumerate_rev_rows()
+        .for_each(|line| add_line_visibility(line, &mut visibility));
+    grid.enumerate_cols()
+        .for_each(|line| add_line_visibility(line, &mut visibility));
+    grid.enumerate_rev_cols()
+        .for_each(|line| add_line_visibility(line, &mut visibility));
 
     println!();
-    println!("{}", visibility_grid);
+    println!("{}", visibility);
 
-    let num_visible = visibility_grid
-        .0
-        .iter()
-        .flat_map(|row| row.iter())
-        .filter(|v| **v != 0)
-        .count();
+    let num_visible = visibility.iter().filter(|v| **v != 0).count();
 
     println!();
     println!("num visible: {}", num_visible);
